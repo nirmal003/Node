4f53ef272a90d23714e4dcbf7bab9cdddf7f43e0
Nirmal <nirmalraj1025@gmail.com>
2024-03-08 10:07:52 +0530

    build

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,23 @@ import "./firebase-messaging-sw";
 
 function App() {
   const [token, setToken] = useState();
+  const [text, setText] = useState();
 
   const send = async () => {
     console.log("hi");
 
+    setText("qwertyuiop");
     setToken(localStorage.getItem("token"));
 
     await axios.post("http://localhost:4000/api/v1/send", {
       fcmToken: token,
-      // "fhdxTc2KHXhdmC8g6WGr_f:APA91bEm3TkybB6YNR4Os0WPEmdpa8n-nEAMPcTCOqZ_nvTvPs4UIxssIbBBf0qJblQtfFAOUxJUDxiMr9Vuyr7eu-L-hCHIshZoLU-5yole8fCQznqezWY74f91vL0Ko2ic8SqDt2w1",
     });
   };
 
   return (
     <div className="App">
       <button onClick={(e) => send()}>send</button>
+      <p>{text && text}</p>
       <p>{token && token}</p>
     </div>
   );
